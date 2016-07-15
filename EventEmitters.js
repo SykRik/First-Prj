ac17123862272de099d542a5f76dcdd6e9e1b78b
sykrik <nguyentantruc89@gmail.com>
2016-07-15 16:19:36 +0700

    * Edit EventEmitters.js.

--- EventEmitters.js
+++ EventEmitters.js
@@ -1,12 +1,36 @@
-var events = require('events')
-var emitter = new events.EventEmitter()
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
-emitter.on('knock', function() {
-    console.log('Who\'s there?')
-})
+//  listener #1
+var listener1 = function listener1() {
+    console.log('listener1 executed.');
+}
 
-emitter.om('knock', function () {
-    console.log('Go away!')
-})
+//  listener #2
+var listener2 = function listener2() {
+    console.log('listener2 executed.');
+}
 
-emitter.emit('knock')
+//  Bind the connection event with the listener1 function
+eventEmitter.addListener('connection', listener1);
+
+//  Bind the connection event with the listener2 function
+eventEmitter.on('connection', listener2);
+
+var eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
+console.log(eventListeners + " Listener(s) listening to connection event");
+
+//  Fire the connection event
+eventEmitter.emit('connection');
+
+//  Remove the binding of listener1 function
+eventEmitter.removeListener('connection', listener1);
+console.log('Listener1 will not listen now');
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
+console.log(eventListeners + " Listner(s) listening to connection event");
+
+console.log("Program Ended.");
